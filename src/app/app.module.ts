e439c1ca1711fc71ee6217e82b39cd91a98fb267
Amarnath <88896502+j1999amar@users.noreply.github.com>
2022-11-25 16:45:55 +0530

    navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddPostComponent } from './add-post/add-post.component';
 import { ViewPostComponent } from './view-post/view-post.component';
-
+import { RouterModule, Routes } from '@angular/router';
+import { NavbarComponent } from "./navbar/navbar.component";
+const myRouter:Routes=[{
+  path:"",
+  component:ViewPostComponent
+},
+{
+  path:"addPost",
+  component:AddPostComponent
+}]
 @NgModule({
-  declarations: [
-    AppComponent,
-    AddPostComponent,
-    ViewPostComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        AddPostComponent,
+        ViewPostComponent,
+        NavbarComponent
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        RouterModule.forRoot(myRouter),
+    ]
 })
 export class AppModule { }
